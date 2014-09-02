28760e8a62ab7a8b22dfb913aaaa3627bfd868c5
Artscore Studio <info@artscore-studio.fr>
2014-09-02 17:14:04 +0200

    add qunit tests

---
+++ js/tests/unit/jquery-asfPreload.js
@@ -0,0 +1,9 @@
+$(function(){
+	'use_strict';
+	
+	module('jquery asfPreload plugin')
+	
+	test('should be defined on jquery object', function() {
+		ok($(document.body).asfPreload, 'asfPreload method is defined')
+	})
+})
